0a30004f2e5db5f6e10323ba19e88eedc389fa6b
Daniil Sitdikov <hello.boriskas@gmail.com>
2022-07-06 09:41:17 +0400

    feat: add the ability to use mock

--- src/components/browser-http-client/browser-http-client.ts
+++ src/components/browser-http-client/browser-http-client.ts
@@ -2,15 +2,25 @@ import { BaseHttpClient, CommonQueryProps, QueryProvider } from '@eigenspace/bas
 import { BrowserRequestProvider } from '../browser-request-provider/browser-request-provider';
 import { BrowserFormDataAppender } from '../browser-form-data-appender/browser-form-data-appender';
 
+interface AdditionalProps {
+    isMock: boolean;
+}
+
 export class BrowserHttpClient implements QueryProvider {
     private baseHttpClient: BaseHttpClient<Response>;
+    private isMock = false;
 
-    constructor(baseUrl = '') {
+    constructor(baseUrl = '', { isMock }: AdditionalProps) {
+        this.isMock = isMock;
         const requestProvider = new BrowserRequestProvider();
         this.baseHttpClient = new BaseHttpClient(requestProvider, undefined, BrowserFormDataAppender, baseUrl);
     }
 
     delete<T>(url: string, props?: CommonQueryProps): Promise<T> {
+        if (this.isMock) {
+            return this.baseHttpClient.get<T>(url, props);
+        }
+
         return this.baseHttpClient.delete<T>(url, props);
     }
 
@@ -19,14 +29,26 @@ export class BrowserHttpClient implements QueryProvider {
     }
 
     patch<T>(url: string, props?: CommonQueryProps): Promise<T> {
+        if (this.isMock) {
+            return this.baseHttpClient.get<T>(url, props);
+        }
+
         return this.baseHttpClient.patch<T>(url, props);
     }
 
     post<T>(url: string, props?: CommonQueryProps): Promise<T> {
+        if (this.isMock) {
+            return this.baseHttpClient.get<T>(url, props);
+        }
+
         return this.baseHttpClient.post<T>(url, props);
     }
 
     put<T>(url: string, props?: CommonQueryProps): Promise<T> {
+        if (this.isMock) {
+            return this.baseHttpClient.get<T>(url, props);
+        }
+
         return this.baseHttpClient.put<T>(url, props);
     }
 }
